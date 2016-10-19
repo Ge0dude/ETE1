e6c4214434d18b40e12cb0f453b3a0fa2a6f5d0f
Brendon <btucker1992@gmail.com>
2016-10-19 11:19:08 +0800

    added the "session" package, not defualt anymore.

--- client/Accounts/LoginModal.js
+++ client/Accounts/LoginModal.js
@@ -1,4 +1,8 @@
-/* Click event doesn't seem to be working?  */
+/* Click event doesn't seem to be working? 
+The lack of functionality has to be coming from here?
+aparently session.set is not a default package anymore...
+yup, added session... :) yay but ugh*/ 
+
 
 
 Template.LoginModal.events({
