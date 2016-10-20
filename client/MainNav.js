b4a28d960e9c6eb78998b3fe472fe0847ea38492
Brendon <btucker1992@gmail.com>
2016-10-20 10:15:28 +0800

    slowly cleaning up CSS

--- client/MainNav.js
+++ client/MainNav.js
@@ -1,3 +1,11 @@
+/*aparently session.set is not a default package anymore...*/
+/*Changing to side nav for a min to check
+okay it works on side nav, it isn't pretty but it works! Need to set hover to pointer and add some styles.
+Or I could just add this all to the header... that makes more sense to me.
+*/
+
+
+
 Template.MainNav.events({
 	'click .login-toggle': ()=> {
 		Session.set('nav-toggle', 'open');
